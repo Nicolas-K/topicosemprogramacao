387c0dcb084aedcc65fc9a14645821651ddc57c8
Bianca <byaam@hotmail.com>
2018-12-02 17:36:53 -0200

    Tela de Login

---
+++ public/javascripts/controllers/login.controller.js
@@ -0,0 +1,10 @@
+function login(){
+    var email = $("#inputEmail").val();
+    var password = $("#inputPassword").val();
+
+    console.log(email);
+    console.log(password);
+
+    console.log("alguma coisa");
+}
+
