14fc3b3b02ca37805fa4751263057768845056a4
Gabsjorge <42352564+Gabsjorge@users.noreply.github.com>
2018-12-03 00:48:28 -0200

    Novas alterações, agora captando a data

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('.cpf-mask').mask('000.000.000-00', {reverse: true});
+});
+
 function exibeUsuarios() {
     $.ajax({
         url: '/users/showAll',
@@ -9,12 +13,9 @@ function exibeUsuarios() {
         },
 
         success: function (call) {
-
             if (call.status === 404 || call.status === 500) {
                 alert('Erro: ' + call.data);
-            }
-
-            else {
+            } else {
                 var i;
                 var user;
                 var userData;
@@ -53,7 +54,6 @@ function exibeUmUsuario(emailhtml) {
         },
 
         success: function (call) {
-
             if (call.status === 404 || call.status === 500) {
                 alert('Erro: ' + call.data);
                 document.getElementById('result').innerHTML += '<br>ACONTECEU 2<br>';
@@ -68,7 +68,6 @@ function exibeUmUsuario(emailhtml) {
                     user = call[i];
 
                     if (emailhtml === user.email) {
-
                         userData = 'ID: ' + user._id +
                             '<br>Nome: ' + user.name +
                             '<br>Email: ' + user.email +
@@ -100,14 +99,11 @@ function exibeNomeUsuario(emailhtml) {
             document.getElementById('result').innerHTML += '<br>ACONTECEU 1<br>';
         },
 
-        success: function (call) {
-
+        success: function (call) {    
             if (call.status === 404 || call.status === 500) {
                 alert('Erro: ' + call.data);
                 document.getElementById('result').innerHTML += '<br>ACONTECEU 2<br>';
-            }
-
-            else {
+            } else {
                 var i;
                 var user;
 
@@ -115,9 +111,10 @@ function exibeNomeUsuario(emailhtml) {
                     user = call[i];
 
                     if (emailhtml === user.email) {
+                        var birthDate = moment(user.birth).format('YYYY-MM-DD');
+                        $('#birthDate').val(birthDate);
 
                         document.getElementById('fullName').value = user.name;
-                        document.getElementById('birthDate').value = user.birth;
                         document.getElementById('password').value = user.password;
                         document.getElementById('email').value = user.email;
                         document.getElementById('nivelEscolaridade').value = user.education;
@@ -132,7 +129,7 @@ function exibeNomeUsuario(emailhtml) {
 
 function inscreveUsuarios(idEvento, emailUsuario) {
     var input = {
-                    "email": emailUsuario
+                'email': emailUsuario
                 };
 
         document.getElementById('result').innerHTML += input;
@@ -142,19 +139,100 @@ function inscreveUsuarios(idEvento, emailUsuario) {
         type: 'PUT',
         dataType: 'json',
         data: input,
-
         error: function (call) {
+            alert('Erro: ' + call.data);
+        },
+
+        success: function (call) {
+
             if (call.status === 404 || call.status === 500 || call.status === 409) {
                 alert('Erro: ' + call.data);
             }
 
             else {
-                alert('Inscrição Efetuada com Sucesso!');
+                alert('DEU CERTO CARAI AOSUDASUD');
             }
-        },
-
-        success: function (call) {
-            alert('Erro: ' + call.data);
         }
     });
 }
+
+/*
+function deletaUsuario() {
+    $.ajax({
+        url: '/users/delete/',
+        type: 'DELETE',
+        dataType: 'json',
+
+        error: function (call) {
+            alert('Erro: ' + call.data);
+            document.getElementById('result').innerHTML += '<br>ACONTECEU 1<br>';
+        },
+
+        success: function (call) {
+
+            if (call.status === 404 || call.status === 500) {
+                alert('Erro: ' + call.data);
+                document.getElementById('result').innerHTML += '<br>ACONTECEU 2<br>';
+            }
+
+            else {
+                var i;
+                var user;
+                var userData;
+
+                for (i = 0; i < call.length; i++) {
+                    user = call[i];
+
+                    userData = 'ID: ' + user._id +
+                        '<br>Nome: ' + user.name +
+                        '<br>Email: ' + user.email +
+                        '<br>Senha: ' + user.password +
+                        '<br>Data de Nascimento: ' + user.birth +
+                        '<br>Educação: ' + user.education +
+                        '<br>Interesse: ' + user.interest +
+                        '<br>CPF/RG: ' + user.CPF_RG +
+                        '<br>Inscrições: ' + user.subscriptions;
+
+                    document.getElementById('result').innerHTML += userData + '<br><br>';
+                    //                    location.reload();
+                }
+            }
+        }
+    });
+}
+*/
+/*
+function deletaCliente(id) {
+    $.ajax({
+        url: '/cliente/deleta?id=' + id,
+        type: 'post',
+        dataType: 'json',
+        error: function (dados) {
+            alert('Erro: ' + dados.data);
+        },
+        success: function (dados) {
+            if (dados.status === 'ERRO')
+    alert('Erro: ' + dados.data);
+            else {
+                var divResult = document.getElementById('result');
+    divResult.removeChild(document.getElementById(id));
+                alert(dados.data);
+                location.reload();
+            }
+        }
+    });
+
+    }
+
+    function exibeClientes(clientes) {
+        for (var i = 0; i < clientes.length; i++) {
+        var cliente = clientes[i];
+        var dadosCliente = 'ID: ' + cliente.id +
+        '<br>Nome: ' + cliente.nome +
+        '<br>Endereço: ' + cliente.endereco +
+        '<br>Telefone: ' + cliente.telefone +
+        '<br>Email: ' + cliente.email +
+        '<br><a href="#" onclick="deletaCliente(' + cliente.id + ');"> EXCLUIR</a>';
+        document.getElementById('result').innerHTML += dadosCliente + '<br><br>';
+        }
+    } */
