9c7e4744bb163c7af009701a88aa162b5926f5d2
Nícolas Kupka <nic.kupka@hotmail.com>
2018-12-03 00:26:07 -0200

    Integração API com AJAX

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -130,83 +130,31 @@ function exibeNomeUsuario(emailhtml) {
     });
 }
 
-/*
-function deletaUsuario() {
+function inscreveUsuarios(idEvento, emailUsuario) {
+    var input = {
+                    "email": emailUsuario
+                };
+
+        document.getElementById('result').innerHTML += input;
+
     $.ajax({
-        url: '/users/delete/',
-        type: 'DELETE',
+        url: '/users/subscribe/' + idEvento,
+        type: 'PUT',
         dataType: 'json',
+        data: input,
 
         error: function (call) {
-            alert('Erro: ' + call.data);
-            document.getElementById('result').innerHTML += '<br>ACONTECEU 1<br>';
+            if (call.status === 404 || call.status === 500 || call.status === 409) {
+                alert('Erro: ' + call.data);
+            }
+
+            else {
+                alert('Inscrição Efetuada com Sucesso!');
+            }
         },
 
         success: function (call) {
-
-            if (call.status === 404 || call.status === 500) {
-                alert('Erro: ' + call.data);
-                document.getElementById('result').innerHTML += '<br>ACONTECEU 2<br>';
-            }
-
-            else {
-                var i;
-                var user;
-                var userData;
-
-                for (i = 0; i < call.length; i++) {
-                    user = call[i];
-
-                    userData = 'ID: ' + user._id +
-                        '<br>Nome: ' + user.name +
-                        '<br>Email: ' + user.email +
-                        '<br>Senha: ' + user.password +
-                        '<br>Data de Nascimento: ' + user.birth +
-                        '<br>Educação: ' + user.education +
-                        '<br>Interesse: ' + user.interest +
-                        '<br>CPF/RG: ' + user.CPF_RG +
-                        '<br>Inscrições: ' + user.subscriptions;
-
-                    document.getElementById('result').innerHTML += userData + '<br><br>';
-                    //                    location.reload();
-                }
-            }
+            alert('Erro: ' + call.data);
         }
     });
 }
-*/
-/*
-function deletaCliente(id) {
-    $.ajax({
-        url: '/cliente/deleta?id=' + id,
-        type: 'post',
-        dataType: 'json',
-        error: function (dados) {
-            alert('Erro: ' + dados.data);
-        },
-        success: function (dados) {
-            if (dados.status === 'ERRO')
-    alert('Erro: ' + dados.data);
-            else {
-                var divResult = document.getElementById('result');
-    divResult.removeChild(document.getElementById(id));
-                alert(dados.data);
-                location.reload();
-            }
-        }
-    });
-
-    }
-
-    function exibeClientes(clientes) {
-        for (var i = 0; i < clientes.length; i++) {
-        var cliente = clientes[i];
-        var dadosCliente = 'ID: ' + cliente.id +
-        '<br>Nome: ' + cliente.nome +
-        '<br>Endereço: ' + cliente.endereco +
-        '<br>Telefone: ' + cliente.telefone +
-        '<br>Email: ' + cliente.email +
-        '<br><a href="#" onclick="deletaCliente(' + cliente.id + ');"> EXCLUIR</a>';
-        document.getElementById('result').innerHTML += dadosCliente + '<br><br>';
-        }
-    } */
