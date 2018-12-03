501762480a4a9e90d6207be2b744ac499886e44b
Nícolas Kupka <nic.kupka@hotmail.com>
2018-12-03 00:56:16 -0200

    Integração API com AJAX

--- javascripts/users.js
+++ javascripts/users.js
@@ -89,10 +89,10 @@ function exibeUmUsuario(emailhtml) {
     });
 }
 
-function deletaUsuario() {
+function exibeNomeUsuario(emailhtml) {
     $.ajax({
-        url: '/users/delete/',
-        type: 'DELETE',
+        url: '/users/showAll',
+        type: 'GET',
         dataType: 'json',
 
         error: function (call) {
@@ -110,61 +110,104 @@ function deletaUsuario() {
             else {
                 var i;
                 var user;
-                var userData;
 
                 for (i = 0; i < call.length; i++) {
                     user = call[i];
 
-                    userData = 'ID: ' + user._id +
-                        '<br>Nome: ' + user.name +
-                        '<br>Email: ' + user.email +
-                        '<br>Senha: ' + user.password +
-                        '<br>Data de Nascimento: ' + user.birth +
-                        '<br>Educação: ' + user.education +
-                        '<br>Interesse: ' + user.interest +
-                        '<br>CPF/RG: ' + user.CPF_RG +
-                        '<br>Inscrições: ' + user.subscriptions;
+                    if (emailhtml === user.email) {
 
-                    document.getElementById('result').innerHTML += userData + '<br><br>';
-                    //                    location.reload();
+                        document.getElementById('fullName').value = user.name;
+                        document.getElementById('birthDate').value = user.birth;
+                        document.getElementById('password').value = user.password;
+                        document.getElementById('email').value = user.email;
+                        document.getElementById('nivelEscolaridade').value = user.education;
+                        document.getElementById('interests').value = user.interest;
+                        //                    location.reload();
+                    }
                 }
             }
         }
     });
 }
 
-/*
-function deletaCliente(id) {
+function inscreveUsuarios(idEvento, emailUsuario) {
+    var input = {
+                    "email": emailUsuario
+                };
+
+        document.getElementById('result').innerHTML += input;
+
     $.ajax({
-        url: '/cliente/deleta?id=' + id,
-        type: 'post',
+        url: '/users/subscribe/' + idEvento,
+        type: 'PUT',
         dataType: 'json',
-        error: function (dados) {
-            alert('Erro: ' + dados.data);
-        },
-        success: function (dados) {
-            if (dados.status === 'ERRO')
-    alert('Erro: ' + dados.data);
+        data: input,
+
+        error: function (call) {
+            if (call.status === 404 || call.status === 500 || call.status === 409) {
+                alert('Erro: ' + call.data);
+            }
+
             else {
-                var divResult = document.getElementById('result');
-    divResult.removeChild(document.getElementById(id));
-                alert(dados.data);
-                location.reload();
+                alert('Inscrição Efetuada com Sucesso!');
+            }
+        },
+
+        success: function (call) {
+            alert('Erro: ' + call.data);
+        }
+    });
+}
+
+function setCookie(cname, cvalue, exdays){
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function userLogin(emailUsuario, senhaUsuario) {
+        var input = {
+                        "email": emailUsuario,
+                        "password": senhaUsuario
+                    };
+
+        document.getElementById('result').innerHTML += input;
+
+    $.ajax({
+        url: '/users/login/',
+        type: 'POST',
+        dataType: 'json',
+        data: input,
+
+        error: function (call) {
+            alert('Erro 1: ' + call.data);
+        },
+
+        success: function (call) {
+            if (call.status === 404 || call.status === 500 || call.status === 409) {
+                alert('Erro 2: ' + call.data);
+            }
+
+            else {
+                setCookie('email', emailUsuario, 7);
+                alert('Login Efetuado com Sucesso!');
             }
         }
     });
-
-    }
-
-    function exibeClientes(clientes) {
-        for (var i = 0; i < clientes.length; i++) {
-        var cliente = clientes[i];
-        var dadosCliente = 'ID: ' + cliente.id +
-        '<br>Nome: ' + cliente.nome +
-        '<br>Endereço: ' + cliente.endereco +
-        '<br>Telefone: ' + cliente.telefone +
-        '<br>Email: ' + cliente.email +
-        '<br><a href="#" onclick="deletaCliente(' + cliente.id + ');"> EXCLUIR</a>';
-        document.getElementById('result').innerHTML += dadosCliente + '<br><br>';
-        }
-    } */
+}
